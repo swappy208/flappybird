7d2c12f4d2599fa11d772b936d8af8bab6dae7fd
Swapnil Tamrakar <swappy208@gmail.com>
2017-11-20 10:35:24 -0500

    updates but bugs

--- flappybird/src/index.js
+++ flappybird/src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import { store, updateFrame, birdjump, game, states, rungame } from './store/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <App store={store} updateFrame={updateFrame} game={game}/>,
+  document.getElementById('root')
+);
+
+function onpress(evt) {
+
+    switch (game.currentstate) {
+    default:
+    case states.Splash:
+      rungame()
+      birdjump(store.bird)
+      break
+    case states.Game:
+      birdjump(store.bird)
+      break
+    case states.Score:
+      break
+  }
+
+}
+
+document.addEventListener('mousedown', onpress);
