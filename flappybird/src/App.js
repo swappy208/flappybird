5648dec8a034c2c29e0d6d5ffaa8e79ac5560f84
Swapnil Tamrakar <swappy208@gmail.com>
2017-11-20 00:24:24 -0500

    added common.js

--- flappybird/src/App.js
+++ flappybird/src/App.js
@@ -3,11 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import {bg} from './common/sprite';
 import sheet from './res/sheet.png';
+import {width, height} from './common/common';
 
 class App extends Component {
   render() {
+
+    const style = {
+      width: width,
+      height: height
+    }
+
     return (
-      <div className="App">
+      <div className="App" id = "fakingcanvas" style= {style}>
         {bg}
       </div>
     );
---
+++ flappybird/src/common/common.js
@@ -0,0 +1,8 @@
+export var width = window.innerWidth;
+export var height = window.innerWidth;
+
+if (width>=500)
+{
+    width = 320;
+    height: 480;
+}
